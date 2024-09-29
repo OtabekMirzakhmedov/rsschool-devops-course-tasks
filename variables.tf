56ccb908dcbc67adf43097288a886eafbf508328
otabekmirzakhmedov <otabek.mirzakhmedov@gmail.com>
2024-09-29 21:29:28 +0500

    making s3 as terraform backend

--- variables.tf
+++ variables.tf
@@ -3,3 +3,15 @@ variable "bucket_name" {
   type        = string
   default     = "mycomponents-tfstate-rs"
 }
+
+variable "key_path" {
+  description = "The key path for the Terraform state file in the S3 bucket"
+  type        = string
+  default     = "myprojects/terraform.tfstate"
+}
+
+variable "region" {
+  description = "The AWS region where the S3 bucket is located"
+  type        = string
+  default     = "ap-south-1"
+}
