b3e26d41b1ba2d164480b12d7b6d347c089d85ac
otabekmirzakhmedov <otabek.mirzakhmedov@gmail.com>
2024-09-29 16:09:10 +0500

    adding different acl

--- s3_bucket.tf
+++ s3_bucket.tf
@@ -1,17 +1,20 @@
 resource "aws_s3_bucket" "terraform_state" {
   bucket = var.bucket_name
-  acl    = "private"
-
   tags = {
     Name        = "Terraform State Bucket"
     Environment = "Dev"
   }
 }
 
+resource "aws_s3_bucket_acl" "terraform_state_acl" {
+  bucket = aws_s3_bucket.terraform_state.id
+  acl    = "private"
+}
+
 resource "aws_s3_bucket_versioning" "versioning" {
   bucket = aws_s3_bucket.terraform_state.id
 
   versioning_configuration {
-    enabled = true
+    status = "Enabled"
   }
 }
