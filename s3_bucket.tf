d7a8f821c958a12ea6f36073988ee5e6cda96fb5
otabekmirzakhmedov <otabek.mirzakhmedov@gmail.com>
2024-09-29 16:05:40 +0500

    changing versioning

--- s3_bucket.tf
+++ s3_bucket.tf
@@ -2,12 +2,16 @@ resource "aws_s3_bucket" "terraform_state" {
   bucket = var.bucket_name
   acl    = "private"
 
-  versioning {
-    enabled = true
-  }
-
   tags = {
     Name        = "Terraform State Bucket"
     Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  versioning_configuration {
+    enabled = true
   }
 }
